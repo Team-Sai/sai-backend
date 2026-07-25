292fb521d7ee60ed81cb560f2c0aadb476722914
yeonchaepark <check4732@naver.com>
2026-07-26 03:34:30 +0900

    Initial commit

---
+++ src/test/java/org/teamsai/saibackend/SaiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.teamsai.saibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
