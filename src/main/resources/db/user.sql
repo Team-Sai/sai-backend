62aecc5a17f8aea7875cb9531421e46ffaecd085
YANG YEEUN <beautifulsilversheep@gmail.com>
2026-08-02 10:51:46 +0900

    feat(#12) sql 추가

--- src/main/resources/db/user.sql
+++ src/main/resources/db/user.sql
@@ -1,19 +1,15 @@
-CREATE TABLE IF NOT EXISTS users (
-    user_id BIGINT NOT NULL AUTO_INCREMENT,
-    user_key VARCHAR(36),
-    user_token VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    birth_date DATE NOT NULL,
-    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-    ON UPDATE CURRENT_TIMESTAMP,
-
-    PRIMARY KEY (user_id),
-    UNIQUE KEY uk_users_user_key (user_key),
-    UNIQUE KEY uk_users_user_token (user_token),
-    UNIQUE KEY uk_users_email (email)
-    ) ENGINE=InnoDB
-    DEFAULT CHARSET=utf8mb4
-    COLLATE=utf8mb4_unicode_ci;
+CREATE TABLE `users` (
+                         `user_id` bigint NOT NULL AUTO_INCREMENT,
+                         `user_key` varchar(64) DEFAULT NULL,
+                         `email` varchar(100) NOT NULL,
+                         `password` varchar(255) NOT NULL,
+                         `name` varchar(50) NOT NULL,
+                         `birth_date` date DEFAULT NULL,
+                         `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                         `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                         `user_token` varchar(50) DEFAULT NULL,
+                         PRIMARY KEY (`user_id`),
+                         UNIQUE KEY `email` (`email`),
+                         UNIQUE KEY `user_token` (`user_token`),
+                         UNIQUE KEY `uk_users_user_key` (`user_key`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
