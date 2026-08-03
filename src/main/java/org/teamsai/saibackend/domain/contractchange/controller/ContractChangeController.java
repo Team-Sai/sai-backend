2675376c20fba3e6b2cc293fc82186d1a4c81807
ehxhfl31436 <ehxhfl31436@gmail.com>
2026-08-03 16:14:48 +0900

    refactor(#30): 계약서와 문제되는 곳 수정 (#33)
    * fix(#13): 상환방식을 계약서 도메인 값(원리금균등상환/원금균등상환/만기일시상환)에 맞춰 드롭다운으로 제한
    
    * refactor(#30): LoanContractService 연동, URL 충돌 해결, SQL 실행순서 수정, 죽은 코드 정리
    
    * feat(#30): 채권자만 변경 요청 가능하도록 검증 추가
    
    * fix(#30): DB 설정 팀 공유값 복원, 채권자 검증 추가, 죽은 코드 정리
    
    * fix(#30): DB 설정 팀 공유값 복원, 검증 순서 수정, user_id FK 추가

--- src/main/java/org/teamsai/saibackend/domain/contractchange/controller/ContractChangeController.java
+++ src/main/java/org/teamsai/saibackend/domain/contractchange/controller/ContractChangeController.java
@@ -7,9 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.teamsai.saibackend.domain.contractchange.dto.request.ContractChangeRequest;
-import org.teamsai.saibackend.domain.contractchange.dto.response.ContractSummaryResponse;
+
 import org.teamsai.saibackend.domain.contractchange.dto.LoanContractChangeDTO;
-import org.teamsai.saibackend.domain.contractchange.dto.LoanContractReadDTO;
+
 import org.teamsai.saibackend.domain.contractchange.service.ContractChangeService;
 
 @RestController
@@ -19,16 +19,7 @@ public class ContractChangeController {
 
     private final ContractChangeService contractChangeService;
 
-    @GetMapping("/{contractId}")
-    public ContractSummaryResponse getContract(
-            @PathVariable Long contractId,
-            @AuthenticationPrincipal(expression = "userId") Long userId
-    ) {
-        LoanContractReadDTO contract = contractChangeService.getContract(contractId, userId);
-        return ContractSummaryResponse.from(contract);
 
-
-    }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/{contractId}/change-requests")
