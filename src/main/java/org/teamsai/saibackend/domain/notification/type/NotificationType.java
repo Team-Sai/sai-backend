b05b6aab4abc5d6b7f6f3446da255ed3588469a1
chaeb24 <jisusushi@gmail.com>
2026-08-12 11:50:49 +0900

    feat(#117):알림센터 추가 완료

--- src/main/java/org/teamsai/saibackend/domain/notification/type/NotificationType.java
+++ src/main/java/org/teamsai/saibackend/domain/notification/type/NotificationType.java
@@ -2,5 +2,6 @@ package org.teamsai.saibackend.domain.notification.type;
 
 public enum NotificationType {
     SETTLEMENT_PARTICIPANT_ADDED,
-    CONTRACT_REQUESTED
+    CONTRACT_REQUESTED,
+    CONTRACT_CHANGE
 }
