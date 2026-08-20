b8111d30399ae3f8012d3db6a8c544c3a321e725
Chaebin <162852897+Chaeb24@users.noreply.github.com>
2026-08-20 17:56:55 +0900

    refactor(#177): 안내 문구 추가
    * refactor(#177): 안내 문구 추가
    
    * refactor(#177):채무자 화면에 문구 추가

--- src/test/java/org/teamsai/saibackend/domain/archive/ArchiveServiceTest.java
+++ src/test/java/org/teamsai/saibackend/domain/archive/ArchiveServiceTest.java
@@ -214,6 +214,7 @@ class ArchiveServiceTest {
                 assertThat(text).contains(contract.getCreditorName());
                 assertThat(text).contains(contract.getDebtorName());
                 assertThat(text).contains(RepaymentMethod.EQUAL_PRINCIPAL_AND_INTEREST.getDescription());
+                assertThat(text).contains("법적 분쟁에 대해 책임을 지지 않습니다");
             }
         }
 
